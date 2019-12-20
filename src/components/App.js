7e80d03f2ff9671c41b59e55d2ca1c2a5a3919b3
Alexander Huber <alih83@gmx.de>
2019-12-20 13:20:47 +0100

    add app proptypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 const ipc = require('electron').ipcRenderer;
 import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -81,6 +82,10 @@ const App = ({ classes }) => {
       </AlertProvider>
     </Provider>
   );
+};
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(App);
